df4b9f0625432ecb7b3b833ad15a4a6fc2edb8de
Ceramicskate0 <ceramicskate0@users.noreply.github.com>
2020-10-03 10:59:31 -0400

    Add files via upload

---
+++ Ceramic/Ceramic/B64.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Ceramic
+{
+    class B64
+    {
+        public static void Base64File(string FilePath)
+        {
+            if (!File.Exists(FilePath))
+            {
+                Console.WriteLine(FilePath + " input file does not exists where you say it is.");
+                Environment.Exit(1);
+            }
+            byte[] bytes = File.ReadAllBytes(FilePath);
+            Console.WriteLine("Writing file to Base64FileOutput.txt");
+            File.WriteAllText("Base64FileOutput.txt", Convert.ToBase64String(bytes));
+            //Console.WriteLine(Convert.ToBase64String(bytes));
+        }
+
+    }
+}
---
+++ Ceramic/Ceramic/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Ceramic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                for (int x = 0; x < args.Length; ++x)
+                {
+                    switch (args[x])
+                    {
+                        case "-b64":
+                            B64.Base64File(args[x + 1]);
+                            break;
+                        case "-xor":
+                            XOREncrypt.XORShellcodeFile(args[x + 1], args[x + 2]);
+                            break;
+                        case "?":
+                            Usage();
+                            break;
+                        case "-h":
+                            Usage();
+                            break;
+                        case "-help":
+                            Usage();
+                            break;
+                    }
+                }
+            }
+            catch
+            {
+                Usage();
+            }
+        }
+
+        static void Usage()
+        {
+            Console.WriteLine(@"
+            CeramicSkate0's 1 stop shop in dotnet core to do random Red Team tasks via 1 app.  
+            Cuz I cant remember all the random commandline ares to do it.      
+            So yes things are case sensitive and yes the commandline inputs must be in order shown below.
+
+            Commandline Params:
+
+            -b64 {Input File Path}
+            The command above will base64 encode a input file and save it to an output file.
+
+            -xor {Input .bin File Path} {XOR KEY}
+            The command above will xor a .bin file with a key and output it to a file. This mean when you un xor it you will need the same key.
+
+                
+            ");
+        }
+    }
+}
