cb1d1c0ded2d4e342f0c1d8f50fb0177c7590be1
ceramic-skate0 <64501203+ceramic-skate0@users.noreply.github.com>
2021-01-26 08:07:59 -0800

    Update AESEncryptShellcode.cs

--- Ceramic/Ceramic/AESEncryptShellcode.cs
+++ Ceramic/Ceramic/AESEncryptShellcode.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -8,36 +9,19 @@ namespace Ceramic
 {
     static class AESEncryptShellcode
     {
-        //private static string aes_iv = "bsxnWolsAyO7kCfWuyrnqg==";
-        //private static string aes_key = "AXe8YwuIn1zxt3FPWTZFlAa14EHdPAdN9FaZ9RQWihc=";
+        private static string aes_iv = "bsxnWolsAyO7kCfWuyrnqg==";
+        private static string aes_key = "AXe8YwuIn1zxt3FPWTZFlAa14EHdPAdN9FaZ9RQWihc=";
         // Convert.FromBase64String(aes_key);
         // Convert.FromBase64String(aes_iv);
-        public static byte[] Prepend(this byte[] bytes, byte[] bytesToPrepend)
-        {
-            var tmp = new byte[bytes.Length + bytesToPrepend.Length];
-            bytesToPrepend.CopyTo(tmp, 0);
-            bytes.CopyTo(tmp, bytesToPrepend.Length);
-            return tmp;
-        }
 
-        public static (byte[] left, byte[] right) Shift(this byte[] bytes, int size)
-        {
-            var left = new byte[size];
-            var right = new byte[bytes.Length - size];
-
-            Array.Copy(bytes, 0, left, 0, left.Length);
-            Array.Copy(bytes, left.Length, right, 0, right.Length);
-
-            return (left, right);
-        }
        
-        public static byte[] Encrypt(byte[] bytesToEncrypt, string password, string iv)
+        public static byte[] Encrypt(byte[] bytesToEncrypt, string password)
         {
             byte[] ivSeed = Guid.NewGuid().ToByteArray();
 
             var rfc = new Rfc2898DeriveBytes(password, ivSeed);
-            byte[] Key = Encoding.Unicode.GetBytes(password);
-            byte[] IV = Encoding.Unicode.GetBytes(iv);
+            byte[] Key = Convert.FromBase64String(aes_key);
+            byte[] IV = Convert.FromBase64String(aes_iv);;
 
             byte[] encrypted;
             using (MemoryStream mstream = new MemoryStream())
@@ -52,13 +36,37 @@ namespace Ceramic
                 encrypted = mstream.ToArray();
             }
 
-            var messageLengthAs32Bits = Convert.ToInt32(bytesToEncrypt.Length);
-            var messageLength = BitConverter.GetBytes(messageLengthAs32Bits);
+            //var messageLengthAs32Bits = Convert.ToInt32(bytesToEncrypt.Length);
+            //var messageLength = BitConverter.GetBytes(messageLengthAs32Bits);
 
-            encrypted = encrypted.Prepend(ivSeed);
-            encrypted = encrypted.Prepend(messageLength);
+            //encrypted = encrypted.Prepend(ivSeed);
+            //encrypted = encrypted.Prepend(messageLength);
 
             return encrypted;
         }
+
+        //c# code to return unencrypted byte code
+        public static byte[] Decrypt(byte[] bytesToDecrypt, string password)
+        {
+            byte[] tmp;
+
+            var length = bytesToDecrypt.Length;
+
+            var rfc = new Rfc2898DeriveBytes(password, Convert.FromBase64String(aes_iv));
+            //byte[] Key = Encoding.Unicode.GetBytes(password);
+
+            byte[] Key = Convert.FromBase64String(aes_key);
+            byte[] IV = Convert.FromBase64String(aes_iv);
+
+            using (MemoryStream mStream = new MemoryStream(bytesToDecrypt))
+            using (AesCryptoServiceProvider aesProvider = new AesCryptoServiceProvider() { Padding = PaddingMode.None })
+            using (CryptoStream cryptoStream = new CryptoStream(mStream, aesProvider.CreateDecryptor(Key, IV), CryptoStreamMode.Read))
+            {
+                cryptoStream.Read(bytesToDecrypt, 0, length);
+                tmp = mStream.ToArray().Take(length).ToArray();
+            }
+            return tmp;
+        }
+
     }
 }
