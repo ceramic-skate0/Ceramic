a5a5acc60cf672b5136323beebd6a95bf7ed7307
Ceramicskate0 <ceramicskate0@users.noreply.github.com>
2021-01-28 01:04:33 -0500

    Add files via upload

---
+++ Ceramic/AESEncryptShellcode.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Ceramic
+{
+    static class AESEncryptShellcode
+    {
+        public static byte[] Encrypt(byte[] bytesToEncrypt, string password,string iv)
+        {
+            byte[] ivSeed = Guid.NewGuid().ToByteArray();
+
+            var rfc = new Rfc2898DeriveBytes(password, ivSeed);
+            byte[] Key = Encoding.Unicode.GetBytes(password);
+            byte[] IV = Encoding.Unicode.GetBytes(iv);
+
+            byte[] encrypted;
+            using (MemoryStream mstream = new MemoryStream())
+            {
+                using (AesCryptoServiceProvider aesProvider = new AesCryptoServiceProvider())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(mstream, aesProvider.CreateEncryptor(Key, IV), CryptoStreamMode.Write))
+                    {
+                        cryptoStream.Write(bytesToEncrypt, 0, bytesToEncrypt.Length);
+                    }
+                }
+                encrypted = mstream.ToArray();
+            }
+
+            //var messageLengthAs32Bits = Convert.ToInt32(bytesToEncrypt.Length);
+            //var messageLength = BitConverter.GetBytes(messageLengthAs32Bits);
+
+            //encrypted = encrypted.Prepend(ivSeed);
+            //encrypted = encrypted.Prepend(messageLength);
+
+            return encrypted;
+        }
+
+        //c# code to return unencrypted byte code
+        public static byte[] Decrypt(byte[] bytesToDecrypt, string password,string iv)
+        {
+            byte[] tmp;
+
+            var length = bytesToDecrypt.Length;
+            byte[] ivSeed = Guid.NewGuid().ToByteArray();
+            var rfc = new Rfc2898DeriveBytes(password, ivSeed);
+            //byte[] Key = Encoding.Unicode.GetBytes(password);
+
+            byte[] Key = Encoding.Unicode.GetBytes(password);
+            byte[] IV = Encoding.Unicode.GetBytes(iv);
+
+            using (MemoryStream mStream = new MemoryStream(bytesToDecrypt))
+            using (AesCryptoServiceProvider aesProvider = new AesCryptoServiceProvider() { Padding = PaddingMode.None })
+            using (CryptoStream cryptoStream = new CryptoStream(mStream, aesProvider.CreateDecryptor(Key, IV), CryptoStreamMode.Read))
+            {
+                cryptoStream.Read(bytesToDecrypt, 0, length);
+                tmp = mStream.ToArray().Take(length).ToArray();
+            }
+            return tmp;
+        }
+
+    }
+}
---
+++ Ceramic/XOREncrypt.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Ceramic
+{
+    class XOREncrypt
+    {
+        // Simple XOR routine from https://github.com/djhohnstein/CSharpCreateThreadExample
+        private static byte[] XorByteArray(byte[] origBytes, char[] cryptor)
+        {
+            byte[] result = new byte[origBytes.Length];
+            int j = 0;
+            for (int i = 0; i < origBytes.Length; i++)
+            {
+                // If we're at the end of the encryption key, move
+                // pointer back to beginning.
+                if (j == cryptor.Length - 1)
+                {
+                    j = 0;
+                }
+                // Perform the XOR operation
+                byte res = (byte)(origBytes[i] ^ Convert.ToByte(cryptor[j]));
+                // Store the result
+                result[i] = res;
+                // Increment the pointer of the XOR key
+                j += 1;
+            }
+            // Return results
+            return result;
+        }
+        public static void XORShellcodeFile(string FileLocation, string key)
+        {
+            if (!File.Exists(FileLocation))
+            {
+                Console.WriteLine("Could not find path to shellcode bin file: {0}", FileLocation);
+                Environment.Exit(1);
+            }
+            byte[] shellcodeBytes = File.ReadAllBytes(FileLocation);
+            // This is the encryption key. If changed, must also be changed in the
+            // project that runs the shellcode.
+            char[] cryptor = key.ToCharArray();
+            byte[] encShellcodeBytes = XorByteArray(shellcodeBytes, cryptor);
+            File.WriteAllBytes("XorShellcode.bin", encShellcodeBytes);
+            Console.WriteLine("Wrote encoded binary to XorShellcode.bin.");
+        }
+    }
+}
